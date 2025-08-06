6215d2e02d5ee7f9f72a45c793e0db4d7cc2635a
Arslan Javaid <aja118379@gmail.com>
2025-08-06 17:09:31 +0500

    Completed HeroSection, started working on DemoSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "@/components/home/hero-section";
+import DemoSection from "@/components/home/demo-section"
 
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection />
-      {/* <DemoSection /> */}
+      <div className="flex flex-col ">
+        <HeroSection />
+        <DemoSection />
+      </div>
       {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
---
+++ src/components/home/demo-section.tsx
@@ -0,0 +1,16 @@
+import { Pizza } from "lucide-react"
+
+const DemoSection = () => {
+  return (
+    <section className="relative">
+        <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 ">
+            <div>
+                <Pizza className="w-6 h-6 text-rose-500" />
+                <h3>Watch how Sommaire transforms this Next.js course PDF into an easy-to-read summary</h3>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default DemoSection
